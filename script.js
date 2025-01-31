3703a7aae974529bbe2aa8643442a778bb198e03
neeluchoudhary930 <neeluchoudhary86@gmail.com>
2025-01-31 15:47:24 +0530

    add all files

---
+++ script.js
@@ -0,0 +1,25 @@
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+let btn5 = document.querySelector("#btn5");
+
+btn1.addEventListener("click", () =>{
+    document.body.style.backgroundColor = "blue";
+})
+
+btn2.addEventListener("click", () =>{
+    document.body.style.backgroundColor = "yellow";
+})
+
+btn3.addEventListener("click", () =>{
+    document.body.style.backgroundColor = "brown";
+})
+
+btn4.addEventListener("click", () =>{
+    document.body.style.backgroundColor = "palevioletred";
+})
+
+btn5.addEventListener("click", () =>{
+    document.body.style.backgroundColor = "green";
+})
